f3f68b64d986285e788d1aa6d1b80e5f7cec8545
murakami <halpygg@gmail.com>
2023-04-02 23:07:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intentapp/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+    }
+}
