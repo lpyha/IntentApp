5f7d99a47c611d2bb8aef890730121ce15bab4f7
murakami <halpygg@gmail.com>
2023-04-03 13:43:04 +0900

    editTextの内容を送る

--- app/src/main/java/com/example/intentapp/MainActivity.java
+++ app/src/main/java/com/example/intentapp/MainActivity.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Intent intent = getIntent();
+        String textData = intent.getStringExtra("textData2");
+        editText.setText(textData);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String text = editText.getText().toString();
                 Intent intent = new Intent(getApplication(), MainActivity2.class);
+                intent.putExtra("textData", text);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/intentapp/MainActivity2.java
+++ app/src/main/java/com/example/intentapp/MainActivity2.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -15,11 +16,17 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Button button2 = (Button) findViewById(R.id.button2);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        Intent intent = getIntent();
+        String textData = intent.getStringExtra("textData");
+        editText2.setText(textData);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent2 = new Intent(getApplication(), MainActivity.class);
+                String textData2 = editText2.getText().toString();
+                intent2.putExtra("textData2", textData2);
                 startActivity(intent2);
             }
         });
